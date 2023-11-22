f14c56ad9fc6013ef059d151186fd326d657a240
evesteve94 <145438481+evesteve94@users.noreply.github.com>
2023-11-22 09:41:46 +0100

    Modals (#1)
    * made a new scripts that would connect to multiple modals
    
    * small changes to background of the modals

--- script/modal-portfolio.js
+++ script/modal-portfolio.js
@@ -1,24 +1,30 @@
 // Hämtar infon från alla modal-element
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("openModalBtn");
-const closeModalBtn = document.getElementById("closeModalBtn");
+//In this example, I'm using the document.querySelectorAll method with the class selector (".modal") to select all elements with the class "modal". The openModalBtns and closeModalBtns are selected using attribute selectors that look for elements with IDs starting with "openModalBtn" and "closeModalBtn" respectively. The [id^='...'] in the attribute selector means "starts with." The loop then adds event listeners for each modal.
+const modals = document.querySelectorAll(".modal");
+const openModalBtns = document.querySelectorAll("[id^='openModalBtn']");
+const closeModalBtns = document.querySelectorAll("[id^='closeModalBtn']");
 
-// visar modalen när knappen trycks -> ändrar class
-btn.onclick = function() {
-    modal.classList.add('modal-showing');
-};
+// Loop through each modal and add event listeners
+modals.forEach((modal, index) => {
+    const openModalBtn = openModalBtns[index];
+    const closeModalBtn = closeModalBtns[index];
 
-// stänger med stäng-knappen -> ändrar class
-closeModalBtn.onclick = function() {
-    modal.classList.remove('modal-showing');
-    modal.classList.add('modal');
-};
+    // visar modalen när knappen trycks -> ändrar class
+    openModalBtn.onclick = function () {
+        modal.classList.add("modal-showing");
+    };
 
-// stänger när användaren klickar utanför modalen
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.classList.remove('modal-showing');
-        modal.classList.add('modal');
-    }
-};
+    // stänger med stäng-knappen -> ändrar class
+    closeModalBtn.onclick = function () {
+        modal.classList.remove("modal-showing");
+        modal.classList.add("modal");
+    };
 
+    // stänger när användaren klickar utanför modalen
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.classList.remove("modal-showing");
+            modal.classList.add("modal");
+        }
+    };
+});
